57136e02fcf3c71fd50e9fa84e51de16ad9aca6b
EowynStark <andwellm89@gmail.com>
2024-03-16 16:16:29 -0500

    adding image asset and header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <img src=" C:\Bootcamp\React-Portfolio\React-Portfolio\src\assets\download20231004145455.png" alt ="illustrated art"/>
+            <h1>Andria Jean </h1>
+        </header>
+    );
+}
+
+export default Header;
