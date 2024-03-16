2557e25d11aabfae4f6dc29f9df65b696d83c129
EowynStark <andwellm89@gmail.com>
2024-03-16 16:41:13 -0500

    adjusting syntax and adding about file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
+import About from './components/About';
 // add further paths once the files are built out
 import './App.css'
 
@@ -10,6 +11,8 @@ function App() {
     <Router>
       <div className='App'>
         <Navigation />
+        <Header />
+        <Route exact path='/' component={About} />
         {/* add further paths once the files are built out */}
         </div>
     </Router>
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section id="about">
+            <h2>About Me</h2>
+            <div className="about-content">
+                <div className="about-text">
+                    <p>
+                        I am an aspiring full stack web developer with a background in creative writing. I have a passion for problem solving, creative endeavors, and learning as much as I can. Being a dedicated learner, I am always looking for ways to improve my skills. I am a team player and am always willing to help others. I am excited to bring my unique background and skills to a new team. Let's brainstorm the next big thing together!
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
