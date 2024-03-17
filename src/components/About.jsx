7a1e53b419404eb5e73f819a168bc42d304000dd
EowynStark <andwellm89@gmail.com>
2024-03-17 08:25:20 -0500

    adjusting syntax and adding a line for about file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ function About() {
             <h2>About Me</h2>
             <div className="about-content">
                 <div className="about-text">
+                    <p>Welcome to my portfolio! Explore my page to get to know me and what I can bring to the virtual table.</p>
                     <p>
                         I am an aspiring full stack web developer with a background in creative writing. I have a passion for problem solving, creative endeavors, and learning as much as I can. Being a dedicated learner, I am always looking for ways to improve my skills. I am a team player and am always willing to help others. I am excited to bring my unique background and skills to a new team. Let's brainstorm the next big thing together!
                     </p>
