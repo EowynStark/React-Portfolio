2af9362b2bb79478e0d2684813b9a3f9abe518d4
EowynStark <andwellm89@gmail.com>
2024-03-19 19:31:13 -0500

    adding bootstrap syntax for style

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -22,30 +22,36 @@ function Contact() {
     };
 
     return (
-        <section id='contact'>
-            <h2>Contact Me</h2>
-            <form id='contact-form' onSubmit={handleSubmit}>
-                <div>
+        <div className='p-3 mb-2 bg-warning text-dark'>
+            <div className='p-3 mb-2 bg-warning text-center text-dark card'>            <h2>Contact Me</h2>
+            <p>Feel free to reach out to me with any questions or opportunities!</p>
+            <p>Fill out the form below to get in touch with me*.</p>
+            <form className='mb-3' id='contact-form' onSubmit={handleSubmit}>
+                <div className='row mb-3'>
+                <div className='row'>
                     <label htmlFor='name'>Name:</label>
                     <input type='text' name='name' value={formData.name} onChange={handleChange} />
                 </div>
-                <div>
+                <div className='row'>
                     <label htmlFor='email'>Email address:</label>
                     <input type='email' name='email' value={formData.email} onChange={handleChange} />
                 </div>
-                <div>
+                <div className='row'>
                     <label htmlFor='message'>Message:</label>
                     <textarea name='message' rows='5' value={formData.message} onChange={handleChange} />
                 </div>
-                <button type='submit'>Submit</button>
+                <button type='submit' className='btn btn-secondary disabled col'>Submit</button>
+                </div>
             </form>
-            <div className='contact-info'>
+            <p>*Submit button disabled until contact functionality is enabled</p>
+            </div>
+            <div className='p-3 mb-2 bg-warning text-center text-dark card' style={{width:'20rem'}}>
                 <h3>Contact Information</h3>
                 <p><strong>Email: </strong> andwellm89@gmail.com</p>
                 <p><strong>Instagram:</strong> @EowynStark</p>
                 <p><strong>GitHub:</strong>EowynStark</p>
             </div>
-        </section>
+        </div>
     );
 }
 
